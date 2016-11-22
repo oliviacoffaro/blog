57787a124009d7e4f9dc8ba37d0e6d56364c3ac4
Olivia Coffaro <ofcoffaro@gmail.com>
2016-11-22 14:54:24 -0500

    sessions controller and created registration form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to 'root_path'
   end
 
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
