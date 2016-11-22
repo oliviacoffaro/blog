434c55e5e68ad9ff299ee63e74d2cbfdcfa8596f
Olivia Coffaro <ofcoffaro@gmail.com>
2016-11-22 11:54:32 -0500

    comments model and schema

--- db/migrate/20161122164237_create_comments.rb
+++ db/migrate/20161122164237_create_comments.rb
@@ -1,6 +1,6 @@
-class CreatePosts < ActiveRecord::Migration[5.0]
+class CreateComments < ActiveRecord::Migration[5.0]
   def change
-    create_table :posts do |t|
+    create_table :comments do |t|
 
       t.integer :user_id, null: false
       t.string :title, null: false
