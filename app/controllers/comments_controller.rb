e5d55b08acd3158ec19b102bc5a37ad23c1fae16
Olivia Coffaro <ofcoffaro@gmail.com>
2016-11-23 16:23:38 -0500

    partial form for comments, and comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to '/'
+    else
+      flash[:notice] = "Please complete all fields."
+   end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:title, :content)
+  end
+
 end
