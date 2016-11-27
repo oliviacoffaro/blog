38f2a802d36364660551bfb7b4185452e8ff90fd
Olivia Coffaro <ofcoffaro@gmail.com>
2016-11-27 12:37:13 -0500

    conversation model

--- db/migrate/20161125185759_create_messages.rb
+++ db/migrate/20161125185759_create_messages.rb
@@ -2,9 +2,10 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
 
-      t.integer :sender
-      t.integer :receiver
-      t.text :content
+        t.text :content
+        t.references :conversation, index: true
+        t.references :user, index: true
+        t.boolean :read, :default => false
 
       t.timestamps
     end
