be156d95444cea6bae726db0d5e0d44b50974fa5
Olivia Coffaro <ofcoffaro@gmail.com>
2016-11-28 17:52:12 -0500

    prepping for amazing s3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+ before_action :set_user, only: [:show, :edit, :update, :destroy]
+ before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
 
   def new
     @user = User.new
@@ -21,4 +23,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def set_s3_direct_post
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+  end
+
 end
