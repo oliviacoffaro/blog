fe85f65b9aa82908b85a64de235e666f391c8191
Olivia Coffaro <ofcoffaro@gmail.com>
2016-11-30 12:31:11 -0500

    changes to post dimensions, put user photo on direct message screen

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   include PostsHelper
 
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
+
+
   def index
     @posts = Post.all
   end
@@ -45,7 +49,15 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :img_url)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def set_s3_direct_post
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
   end
 
 end
