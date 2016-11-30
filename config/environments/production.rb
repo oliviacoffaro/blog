f4ed14444774d2908102145d9a2624ba7a75513c
Olivia Coffaro <ofcoffaro@gmail.com>
2016-11-30 16:48:02 -0500

    emoji heroku fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
@@ -71,6 +71,8 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.digest = true
+
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
